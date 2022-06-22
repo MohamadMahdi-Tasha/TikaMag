c39cf945e9306396c06a572c3453e71b662a53a5
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-06-22 13:21:42 +0430

    added structure of left side of blog

---
+++ assets/base/js/swipers.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".dv-related-things-swiper", {
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
