003a58f8ba9ede5a8b74ef04719f0a146a7ca545
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-07-07 10:31:14 +0430

    added mq of blog section

--- assets/base/js/swipers.js
+++ assets/base/js/swipers.js
@@ -1,3 +1,5 @@
+// Configs Of Sliders
+const swiper2 = new Swiper(".dv-2slide-per-view-slider", {slidesPerView: 2,})
 const swiper = new Swiper(".dv-related-things-swiper", {
     pagination: {
         el: '.swiper-pagination',
