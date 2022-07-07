65f624c255a9c2410ff5d985e0439e32a12b145d
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-07-07 17:53:15 +0430

    added styles of first section

--- assets/base/js/swipers.js
+++ assets/base/js/swipers.js
@@ -1,13 +1,4 @@
 // Configs Of Sliders
-const swiper2 = new Swiper(".dv-2slide-per-view-slider-with-gap", {
-    slidesPerView: 2,
-    spaceBetween: 50,
-    breakpoints: {
-        767: {spaceBetween: 50, slidesPerView: 2,},
-        1: {spaceBetween: 10,slidesPerView: 1,}
-    }
-})
-
 // TODO: Add Timer To Them
 const swiper = new Swiper(".dv-related-things-swiper", {
     slidesPerView: 1,
@@ -21,3 +12,26 @@ const swiper = new Swiper(".dv-related-things-swiper", {
         prevEl: '.swiper-button-prev',
     },
 });
+
+const swiper2 = new Swiper(".dv-2slide-per-view-slider-with-gap", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    breakpoints: {
+        767: {spaceBetween: 50, slidesPerView: 2,},
+        1: {spaceBetween: 10,slidesPerView: 1,}
+    }
+})
+
+const swiper3 = new Swiper(".dv-home-first-section-slider", {
+    slidesPerView: 3,
+    spaceBetween: 20,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        type: 'bullets',
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+})
