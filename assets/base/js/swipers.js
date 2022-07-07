43fdf660b2b244b8771f05bf96b2ce91d0e02ece
MohamadMahdi-Tasha <98010276+MohamadMahdi-Tasha@users.noreply.github.com>
2022-07-07 11:21:01 +0430

    fixed some bugs

--- assets/base/js/swipers.js
+++ assets/base/js/swipers.js
@@ -1,6 +1,13 @@
 // Configs Of Sliders
 const swiper2 = new Swiper(".dv-2slide-per-view-slider", {slidesPerView: 2,})
-const swiper3 = new Swiper(".dv-2slide-per-view-slider-with-gap", {slidesPerView: 2, spaceBetween: 50})
+const swiper3 = new Swiper(".dv-2slide-per-view-slider-with-gap", {
+    slidesPerView: 2,
+    spaceBetween: 50,
+    breakpoints: {
+        767: {spaceBetween: 50,},
+        1: {spaceBetween: 10,}
+    }
+})
 const swiper = new Swiper(".dv-related-things-swiper", {
     pagination: {
         el: '.swiper-pagination',
